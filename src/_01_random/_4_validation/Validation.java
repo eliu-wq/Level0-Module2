d7459994a8cce6fd3f646ba752e409cc82992d69
lvl0_module2 <Eddiejhunter1000000@gmail.com>
2026-06-13 11:27:58 -0700

    finished safe cracker

--- src/_01_random/_4_validation/Validation.java
+++ src/_01_random/_4_validation/Validation.java
@@ -9,17 +9,34 @@ import javax.swing.JOptionPane;
 
 public class Validation {
 	public static void main(String[] args) {
-		
+
 		Random randomMaker = new Random();
-		
+
 		int randomNumber = randomMaker.nextInt(5);
-		
+
 		System.out.println(randomNumber);
 
 		// 1. Use each value of randomNumber to give the user a random compliment.
 
-		// 2. Repeat all the code above 10 times
-		
-		// 3. Find someone to test out your program. They will like it :)
+		for (int i = 0; i < 10; i++) {
+			if (randomNumber == 0) {
+				System.out.print("Nice shoes");
+			} else if (randomNumber == 1) {
+				System.out.print("Nice hair");
+			} else if (randomNumber == 2) {
+				System.out.print("Nice shirt");
+			} else if (randomNumber == 3) {
+				System.out.print("Nice pants");
+			} else if (randomNumber == 4) {
+				System.out.print("Nice socks");
+			}
+			if (i < 9) {
+			System.out.print(", ");
+			}
+
+			// 2. Repeat all the code above 10 times
+
+			// 3. Find someone to test out your program. They will like it :)
+		}
 	}
 }
--- src/_02_methods/_2_safe_cracker/SafeCracker.java
+++ src/_02_methods/_2_safe_cracker/SafeCracker.java
@@ -29,11 +29,13 @@ public class SafeCracker {
 		 * Your mission: use the tryCode method to crack the safe by trying all possible
 		 * combinations
 		 */		
+		for (int i = 0; ; i++) {
+			tryCode(i);
+		}
 
 	}
 
 	 void tryCode(int guess) {
-		System.out.println("trying " + guess);
 
 		int secretCode = 9999999 - wekncrzpasfdkjhcfjse;
 
